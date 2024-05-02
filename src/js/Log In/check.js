b68d0d947a38c9960a8902b1520a7f2ce1780759
Solomka Andrii <andriissolomka@gmail.com>
2024-05-02 15:32:09 +0300

    do modules

--- src/js/Log In/check.js
+++ src/js/Log In/check.js
@@ -1,68 +1,8 @@
 'use strict';
 
-import {
-  URL,
-  logInBtn,
-  getUserName,
-  signUpBtn,
-  getUserAge,
-  getUserEmail,
-  signInBtn,
-  getUserPassword,
-} from '../const/const.js';
-import { getUserData } from '../main/main.js';
-
-const getUserInformation = async (URL) => {
-  const data = await getUserData(URL);
-  const names = data.map((person) => person.name);
-  const passwords = data.map((person) => person.password);
-  const emails = data.map((person) => person.email);
-  setupNameInput(names);
-  setupPassword(passwords);
-  setupEmail(emails);
-  setupAge();
-};
-
-let newUser = {
-  name: '',
-  email: '',
-  password: '',
-  age: '',
-};
-
-const setupNameInput = (names) => {
-  getUserName.addEventListener('input', (event) => {
-    const inputName = event.target.value.trim();
-    const isNameTaken = names.includes(inputName);
-    const correctName = checkName(inputName);
-
-    if (!isNameTaken && correctName) {
-      logInBtn.style.backgroundColor = '';
-      newUser.name = inputName;
-    } else {
-      logInBtn.style.backgroundColor = 'red';
-    }
-  });
-};
-
 const checkName = (name) => {
   const nameRegex = /^[a-zA-Z]{3,12}$/;
   return nameRegex.test(name);
-};
-
-const setupPassword = (passwords) => {
-  getUserPassword.addEventListener('input', (event) => {
-    const inputPassword = event.target.value.trim();
-    const isPasswordTaken = passwords.includes(inputPassword);
-    const correctPassword = checkPassword(inputPassword);
-
-    if (!isPasswordTaken && correctPassword) {
-      logInBtn.style.backgroundColor = '';
-      newUser.password = inputPassword;
-    } else {
-      logInBtn.style.backgroundColor = 'red';
-    }
-  });
 };
 
 const checkPassword = (password) => {
@@ -116,21 +56,6 @@ const checkPassword = (password) => {
   return options.status;
 };
 
-const setupEmail = (emails) => {
-  getUserEmail.addEventListener('input', (event) => {
-    const inputEmail = event.target.value.trim();
-    const isEmailTaken = emails.includes(inputEmail);
-    const correctEmail = checkEmail(inputEmail);
-
-    if (!isEmailTaken && correctEmail) {
-      logInBtn.style.backgroundColor = '';
-      newUser.email = inputEmail;
-    } else {
-      logInBtn.style.backgroundColor = 'red';
-    }
-  });
-};
-
 const checkEmail = (email) => {
   const options = {
     maxLength: 50,
@@ -155,38 +80,4 @@ const checkEmail = (email) => {
   return true;
 };
 
-const setupAge = () => {
-  getUserAge.addEventListener('input', (event) => {
-    const inputAge = Number(event.target.value.trim());
-
-    if (inputAge <= 100 && inputAge > 0) {
-      logInBtn.style.backgroundColor = '';
-      newUser.age = inputAge;
-    } else {
-      logInBtn.style.backgroundColor = 'red';
-    }
-  });
-};
-
-signUpBtn.addEventListener('click', (event) => {
-  event.preventDefault();
-  getUserEmail.classList.toggle('hidden');
-  getUserAge.classList.toggle('hidden');
-  signInBtn.classList.toggle('hidden');
-  logInBtn.classList.toggle('hidden');
-});
-
-logInBtn.addEventListener('click', async () => {
-  try {
-    await fetch(URL, {
-      method: 'POST',
-      body: JSON.stringify(newUser),
-    });
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-getUserInformation(URL);
-
-export { setupNameInput };
+export { checkName, checkEmail, checkPassword };
--- src/js/Log In/logIn.js
+++ src/js/Log In/logIn.js
@@ -1,59 +1,112 @@
-/* 'use strict';
+'use strict';
 
 import {
   URL,
-  getUserPassword,
-  signUpBtn,
-  getUserName,
-  hideAge,
-  hideEmail,
   logInBtn,
+  getUserName,
+  signUpBtn,
+  getUserAge,
+  getUserEmail,
   signInBtn,
+  getUserPassword,
 } from '../const/const.js';
-import { checkName } from './check.js';
+import { getUserData } from '../main/main.js';
+import { checkEmail, checkName, checkPassword } from './check.js';
 
-/* signUpBtn.addEventListener('click', (event) => {
+signUpBtn.addEventListener('click', (event) => {
   event.preventDefault();
-  hideEmail.classList.toggle('hidden');
-  hideAge.classList.toggle('hidden');
+  getUserEmail.classList.toggle('hidden');
+  getUserAge.classList.toggle('hidden');
   signInBtn.classList.toggle('hidden');
   logInBtn.classList.toggle('hidden');
+});
 
-  let newUser = {
-    name: '',
-    email: '',
-    password: '',
-    age: '',
-  };
+const getUserInformation = async (URL) => {
+  const data = await getUserData(URL);
+  const names = data.map((person) => person.name);
+  const passwords = data.map((person) => person.password);
+  const emails = data.map((person) => person.email);
+  setupNameInput(names);
+  setupPassword(passwords);
+  setupEmail(emails);
+  setupAge();
+};
 
-  getUserName.value = '';
+let newUser = {
+  name: '',
+  email: '',
+  password: '',
+  age: '',
+};
+
+const setupNameInput = (names) => {
   getUserName.addEventListener('input', (event) => {
-    newUser.name = event.target.value;
-    checkName(newUser.name);
-  });
+    const inputName = event.target.value.trim();
+    const isNameTaken = names.includes(inputName);
+    const correctName = checkName(inputName);
 
-  hideEmail.addEventListener('input', (event) => {
-    newUser.email = event.target.value;
-  });
-
-  getUserPassword.value = '';
-  getUserPassword.addEventListener('input', (event) => {
-    newUser.password = event.target.value;
-  });
-
-  hideAge.addEventListener('input', (event) => {
-    newUser.age = event.target.value;
-  });
-
-  logInBtn.addEventListener('click', async () => {
-    try {
-      await fetch(URL, {
-        method: 'POST',
-        body: JSON.stringify(newUser),
-      });
-    } catch (error) {
-      console.error(error);
+    if (!isNameTaken && correctName) {
+      logInBtn.style.backgroundColor = '';
+      newUser.name = inputName;
+    } else {
+      logInBtn.style.backgroundColor = 'red';
     }
   });
+};
+
+const setupPassword = (passwords) => {
+  getUserPassword.addEventListener('input', (event) => {
+    const inputPassword = event.target.value.trim();
+    const isPasswordTaken = passwords.includes(inputPassword);
+    const correctPassword = checkPassword(inputPassword);
+
+    if (!isPasswordTaken && correctPassword) {
+      logInBtn.style.backgroundColor = '';
+      newUser.password = inputPassword;
+    } else {
+      logInBtn.style.backgroundColor = 'red';
+    }
+  });
+};
+
+const setupEmail = (emails) => {
+  getUserEmail.addEventListener('input', (event) => {
+    const inputEmail = event.target.value.trim();
+    const isEmailTaken = emails.includes(inputEmail);
+    const correctEmail = checkEmail(inputEmail);
+
+    if (!isEmailTaken && correctEmail) {
+      logInBtn.style.backgroundColor = '';
+      newUser.email = inputEmail;
+    } else {
+      logInBtn.style.backgroundColor = 'red';
+    }
+  });
+};
+
+const setupAge = () => {
+  getUserAge.addEventListener('input', (event) => {
+    const inputAge = Number(event.target.value.trim());
+
+    if (inputAge <= 100 && inputAge > 0) {
+      logInBtn.style.backgroundColor = '';
+      newUser.age = String(inputAge);
+    } else {
+      logInBtn.style.backgroundColor = 'red';
+    }
+  });
+};
+
+logInBtn.addEventListener('click', async () => {
+  try {
+    await fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify(newUser),
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
- */
+
+getUserInformation(URL);
+export { setupNameInput };
