ad8d4a16b985e7485eb47ee1a99fe3abb0377092
Solomka Andrii <andriissolomka@gmail.com>
2024-05-15 21:21:20 +0300

    some fixes

--- src/js/Next Page/test.js
+++ src/js/Next Page/test.js
@@ -1,6 +1,3 @@
 'use strict';
 
-const username = localStorage.getItem('username');
-const password = localStorage.getItem('password');
-
-alert(`Hello ${username}, your password is ${password}`);
+alert(`Hello!`);
