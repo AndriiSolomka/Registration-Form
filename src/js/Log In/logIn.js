39c463644a1d5eb509eb7516afba6104004b6246
Solomka Andrii <andriissolomka@gmail.com>
2024-05-25 15:09:20 +0300

    add ternary operators and grouping buttons

--- src/js/Log In/logIn.js
+++ src/js/Log In/logIn.js
@@ -18,13 +18,22 @@ import { generatePassword } from './randomPassword.js';
 
 signUpBtn.addEventListener('click', (event) => {
   event.preventDefault();
-  getUserEmail.classList.toggle('hidden');
-  getUserAge.classList.toggle('hidden');
-  signInBtn.classList.toggle('hidden');
-  logInBtn.classList.toggle('hidden');
-  passwordBtn.classList.toggle('hidden');
-  logInBtn.style.opacity = '0.5';
-  logInBtn.style.pointerEvents = 'none';
+  const elementsToToggle = [
+    getUserEmail,
+    getUserAge,
+    signInBtn,
+    logInBtn,
+    botCheckBTN,
+    passwordBtn,
+  ];
+
+  elementsToToggle.forEach((element) => element.classList.toggle('hidden'));
+  const elementsToDisable = [logInBtn, botCheckBTN];
+
+  elementsToDisable.forEach((element) => {
+    element.style.opacity = '0.5';
+    element.style.pointerEvents = 'none';
+  });
 });
 
 const newUser = {
@@ -38,10 +47,10 @@ const setupNameInput = () => {
   getUserName.addEventListener('input', (event) => {
     const inputName = event.target.value.trim();
     const correctName = checkName(inputName);
+    const resetName = '';
 
-    if (correctName) {
-      newUser.username = inputName;
-    }
+    newUser['username'] = correctName ? inputName : resetName;
+
     validateForm();
   });
 };
@@ -51,10 +60,10 @@ const setupPasswordInput = () => {
   getUserPassword.addEventListener('input', (event) => {
     const inputPassword = event.target.value.trim();
     const correctPassword = checkPassword(inputPassword);
+    const resetPassword = '';
 
-    if (correctPassword) {
-      newUser.password = inputPassword;
-    }
+    newUser['password'] = correctPassword ? inputPassword : resetPassword;
+
     validateForm();
   });
 };
@@ -76,10 +85,10 @@ const setupEmailInput = () => {
   getUserEmail.addEventListener('input', (event) => {
     const inputEmail = event.target.value.trim();
     const correctEmail = checkEmail(inputEmail);
+    const resetEmail = '';
 
-    if (correctEmail) {
-      newUser.email = inputEmail;
-    }
+    newUser['email'] = correctEmail ? inputEmail : resetEmail;
+
     validateForm();
   });
 };
@@ -90,9 +99,11 @@ const setupAgeInput = () => {
     const inputAge = Number(event.target.value.trim());
     const minAge = 5;
     const maxAge = 100;
-    if (inputAge < maxAge && inputAge > minAge) {
-      newUser.age = inputAge;
-    }
+    const isAgeValid = inputAge <= maxAge && inputAge >= minAge;
+    const resetAge = '';
+
+    newUser.age = isAgeValid ? inputAge : resetAge;
+
     validateForm();
   });
 };
@@ -101,7 +112,7 @@ setupAgeInput();
 const validateForm = (human) => {
   const { username, email, password, age } = newUser;
   if (username && email && password && age) {
-    botCheckBTN.classList.toggle('hidden');
+    setButtonState(botCheckBTN, true);
     if (human) {
       doBtnOn();
     }
@@ -110,20 +121,22 @@ const validateForm = (human) => {
   }
 };
 
+const setButtonState = (button, isEnabled) => {
+  button.style.pointerEvents = isEnabled ? 'auto' : 'none';
+  button.style.opacity = isEnabled ? '1' : '0.5';
+};
+
 const doBtnOn = () => {
-  logInBtn.disabled = false;
-  logInBtn.style.pointerEvents = 'auto';
-  logInBtn.style.opacity = '1';
+  setButtonState(logInBtn, true);
 };
 
 const doBtnOff = () => {
-  logInBtn.disabled = true;
-  logInBtn.style.pointerEvents = 'none';
-  logInBtn.style.opacity = '0.5';
+  setButtonState(logInBtn, false);
+  setButtonState(botCheckBTN, false);
 };
 
 logInBtn.addEventListener('click', async () => {
-  checkUserData(newUser);
+  await checkUserData(newUser);
 });
 
 const checkUserData = async (newUser) => {
